a2062c725847a44cd1bebd136914808302980155
SunDwarf <sun@veriny.tf>
2016-07-21 13:18:10 +0100

    0.3.1
    Signed-off-by: SunDwarf <sun@veriny.tf>

--- flask_s3.py
+++ flask_s3.py
@@ -50,7 +50,7 @@ DEFAULT_SETTINGS = {'FLASKS3_USE_HTTPS': True,
                     'FLASKS3_FORCE_MIMETYPE': False,
                     'FLASKS3_PREFIX': ''}
 
-__version__ = (0, 3, 0)
+__version__ = (0, 3, 1)
 
 
 def _get_statics_prefix(app):
