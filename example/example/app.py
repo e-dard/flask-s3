b39f0ad85c78ef141405462da0ed011998f69aa3
Fartash Haghani <fartashh@gmail.com>
2017-06-29 12:14:39 -0400

    add update method to be able to update new commited static files

--- example/example/app.py
+++ example/example/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template_string
 from flask_s3 import FlaskS3, create_all
 
 app = Flask(__name__)
-app.config['S3_BUCKET_NAME'] = 'mybucketname'
+app.config['FLASKS3_BUCKET_NAME'] = 'mybucketname'
 app.config['USE_S3_DEBUG'] = True
 
 s3 = FlaskS3(app)
@@ -13,6 +13,7 @@ def index():
     template_str = """{{ url_for('static', filename="foo.js") }}"""
     return render_template_string(template_str)
 
+@app.route('/upload')
 def upload_all():
     create_all(app, user='MY_AWS_ID', password='MY_AWS_SECRET')
 
